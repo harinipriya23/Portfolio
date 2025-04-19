89652484bf1175319c083a83ad9ebac35b7be097
Harini priya <harinipriya200903@gmail.com>
2025-04-19 19:33:37 +0530

    save

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       <p className="mt-2 w-[300px] px-4 text-center text-[14px] text-purple-800 dark:text-gray-400 sm:w-[350px] sm:text-[18px] md:w-[400px] md:text-[20px]">
         A creative developer turning visions into engaging digital designs.
       </p>
-      <a href="Harinipriya_FResume" download={true} target="_blank">
+      <a href="Harinipriya_FrontendResume" download={true} target="_blank">
         <button className="my-2 rounded-md bg-purple-300 px-4 py-2 font-semibold transition-all duration-300 hover:scale-105 hover:bg-blue-300 dark:bg-gray-800 dark:hover:bg-gray-700">
           Resume
         </button>
